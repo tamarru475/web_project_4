dbf6223752f6b536a7cacec11fae16593950986d
tamarru <tamarrubin475@gmail.com>
2022-04-08 19:41:29 +0930

    add addpopup

--- scripts/index.js
+++ scripts/index.js
@@ -1,44 +1,105 @@
-let popup = document.querySelector(".popup");
-let profileEditbutton = document.querySelector(".profile__edit-button");
-let popupClosebutton = popup.querySelector(".popup__close-button");
-let popupForm = document.querySelector(".form");
+const main = document.querySelector(".main");
+const editPopup = document.querySelector(".edit__popup");
+const profileEditbutton = document.querySelector(".profile__edit-button");
+const editPopupClosebutton = editPopup.querySelector(
+  ".edit__popup-close_button"
+);
+const editForm = document.querySelector(".edit__form");
+const profileAddButton = document.querySelector(".profile__add-button");
+const addPopupTemplate = document.querySelector("#add__popup-template").content;
+const addPopup = addPopupTemplate.querySelector(".add__popup").cloneNode(true);
+const addPopupButton = addPopup.querySelector(".add__popup-close_button");
+const addForm = addPopup.querySelector(".add__form");
 
-function openPopup() {
-  popup.classList.add("popup_active");
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg",
+  },
+];
 
-  let nameInput = document.getElementById("name");
+function openEditPopup() {
+  editPopup.classList.add("edit__popup_active");
 
-  let jobInput = document.getElementById("job");
+  const nameInput = document.getElementById("name");
 
-  let profileName = document.querySelector(".profile__name");
-  let profileJob = document.querySelector(".profile__name-discription");
+  const jobInput = document.getElementById("job");
+
+  const profileName = document.querySelector(".profile__name");
+  const profileJob = document.querySelector(".profile__name-discription");
 
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 }
 
-function closePopup() {
-  popup.classList.remove("popup_active");
+function closeEditPopup() {
+  editPopup.classList.remove("edit__popup_active");
 }
 
-function handleProfileFormSubmit(evt) {
+function handleProfileEditFormSubmit(evt) {
   evt.preventDefault();
 
-  let nameInput = document.getElementById("name");
+  const nameInput = document.getElementById("name");
 
-  let jobInput = document.getElementById("job");
+  const jobInput = document.getElementById("job");
 
-  let profileName = document.querySelector(".profile__name");
-  let profileJob = document.querySelector(".profile__name-discription");
+  const profileName = document.querySelector(".profile__name");
+  const profileJob = document.querySelector(".profile__name-discription");
 
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
 
-  popup.classList.remove("popup_active");
+  editPopup.classList.remove("edit__popup_active");
 }
 
-profileEditbutton.addEventListener("click", openPopup);
+function openAddPopup() {
+  main.append(addPopup);
+}
 
-popupClosebutton.addEventListener("click", closePopup);
+function closeAddPopup() {
+  addPopup.remove();
+}
 
-popupForm.addEventListener("submit", handleProfileFormSubmit);
+function handleProfileAddFormSubmit(evt) {
+  evt.preventDefault();
+
+  const titelInput = document.getElementById("title");
+  const imageInput = document.getElementById("image-link");
+
+  initialCards.unshift({ name: titelInput.value, link: imageInput.value });
+  console.log(initialCards);
+
+  addPopup.remove();
+}
+
+profileEditbutton.addEventListener("click", openEditPopup);
+
+profileAddButton.addEventListener("click", openAddPopup);
+
+editPopupClosebutton.addEventListener("click", closeEditPopup);
+
+addPopupButton.addEventListener("click", closeAddPopup);
+
+editForm.addEventListener("submit", handleProfileEditFormSubmit);
+
+addForm.addEventListener("submit", handleProfileAddFormSubmit);
