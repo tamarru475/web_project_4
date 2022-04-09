be117a1c548cea7dd1d95c9a789b7ef37d0fe51a
tamarru <tamarrubin475@gmail.com>
2022-04-09 20:25:37 +0930

    start of card array

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,4 @@
-const main = document.querySelector(".main");
-const editPopup = document.querySelector(".edit__popup");
-const profileEditbutton = document.querySelector(".profile__edit-button");
-const editPopupClosebutton = editPopup.querySelector(
-  ".edit__popup-close_button"
-);
-const editForm = document.querySelector(".edit__form");
-const profileAddButton = document.querySelector(".profile__add-button");
-const addPopupTemplate = document.querySelector("#add__popup-template").content;
-const addPopup = addPopupTemplate.querySelector(".add__popup").cloneNode(true);
-const addPopupButton = addPopup.querySelector(".add__popup-close_button");
-const addForm = addPopup.querySelector(".add__form");
+//// decleratoins /////
 
 const initialCards = [
   {
@@ -37,6 +26,26 @@ const initialCards = [
     link: "https://code.s3.yandex.net/web-code/lago.jpg",
   },
 ];
+
+///// models////
+const editPopup = document.querySelector(".edit__popup");
+const editForm = document.querySelector(".edit__form");
+const addPopup = document.querySelector(".add__popup");
+const addForm = addPopup.querySelector(".add__form");
+
+/////buttons///
+const profileEditbutton = document.querySelector(".profile__edit-button");
+const editPopupClosebutton = editPopup.querySelector(
+  ".edit__popup-close_button"
+);
+const profileAddButton = document.querySelector(".profile__add-button");
+const addPopupButton = addPopup.querySelector(".add__popup-close_button");
+
+//wrappers///
+
+const cardContainer = document.querySelector(".gallery__container");
+
+////functions////
 
 function openEditPopup() {
   editPopup.classList.add("edit__popup_active");
@@ -73,11 +82,11 @@ function handleProfileEditFormSubmit(evt) {
 }
 
 function openAddPopup() {
-  main.append(addPopup);
+  addPopup.classList.add("add__popup_active");
 }
 
 function closeAddPopup() {
-  addPopup.remove();
+  addPopup.classList.remove("add__popup_active");
 }
 
 function handleProfileAddFormSubmit(evt) {
@@ -89,8 +98,10 @@ function handleProfileAddFormSubmit(evt) {
   initialCards.unshift({ name: titelInput.value, link: imageInput.value });
   console.log(initialCards);
 
-  addPopup.remove();
+  addPopup.classList.remove("add__popup_active");
 }
+
+/////event listeners/////
 
 profileEditbutton.addEventListener("click", openEditPopup);
 
@@ -103,3 +114,20 @@ addPopupButton.addEventListener("click", closeAddPopup);
 editForm.addEventListener("submit", handleProfileEditFormSubmit);
 
 addForm.addEventListener("submit", handleProfileAddFormSubmit);
+
+///template////
+
+initialCards.forEach((card) => {
+  const cardTemplate = document
+    .querySelector("#card-template")
+    .content.querySelector(".gallery__card");
+  const cardElement = cardTemplate.cloneNode(true);
+
+  const cardImage = cardElement.querySelector(".gallery__card-image");
+  const cardTitle = cardElement.querySelector(".gallery__card-place");
+
+  cardImage.src = `url(${card})`;
+  cardTitle.textContent = card.name;
+
+  cardContainer.append(cardElement);
+});
